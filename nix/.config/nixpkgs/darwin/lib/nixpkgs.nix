718d1242dec4a87ea8ac58db0865772bd06cdc60
Ruben Maher <ruben@maher.fyi>
2018-08-23 09:57:04 +0930

    darwin: update

--- nix/.config/nixpkgs/darwin/lib/nixpkgs.nix
+++ nix/.config/nixpkgs/darwin/lib/nixpkgs.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
 
   src = fetchGit {
     url = ../../../../.nix-defexpr/nixpkgs;
-    rev = "a8c71037e041725d40fbf2f3047347b6833b1703";
+    rev = "8395f9aa85e621c076334a67d814de8221ce7983";
   };
 
   dontBuild = true;
