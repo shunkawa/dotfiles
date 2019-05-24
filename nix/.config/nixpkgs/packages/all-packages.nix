05b8f29a9625020b51afd18e841b267e255797b3
Ruben Maher <ruben@maher.fyi>
2019-05-24 14:52:02 +0900

    fix: select select correct sourceRoot fro ImageOptim

--- nix/.config/nixpkgs/packages/all-packages.nix
+++ nix/.config/nixpkgs/packages/all-packages.nix
@@ -298,6 +298,7 @@ in rec {
   ImageOptim = (installApplication rec {
     name = "ImageOptim";
     version = "1.8.8";
+    sourceRoot = "${name}.app";
     src = pkgs.fetchurl {
       url = "https://imageoptim.com/ImageOptim${version}.tar.bz2";
       sha256 = "1qyc4fpx9bac3mi02gracv86c4xq1b6is3am79p1iw6bp19fd37l";
