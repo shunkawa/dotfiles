90e8cf48775a0e7ef875d3c8b44e576ca36412e2
Ruben Maher <ruben@maher.fyi>
2019-11-01 12:28:26 +0900

    feat: add some fzf git helpers

--- nix/.config/nixpkgs/home/config/zsh/default.nix
+++ nix/.config/nixpkgs/home/config/zsh/default.nix
@@ -441,6 +441,24 @@ in (lib.recursiveUpdate ({
     # For per-directory or project configuration (private registries, etc), do
     # something like this in .envrc:
     # export NPM_CONFIG_USERCONFIG="$(pwd)/npmrc"
+
+    join-lines() {
+      local item
+      while read item; do
+        echo -n "''${(q)item} "
+      done
+    }
+
+    bind-git-helper() {
+      local c
+      for c in $@; do
+        eval "fzf-g$c-widget() { local result=\$(g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+        eval "zle -N fzf-g$c-widget"
+        eval "bindkey '^g^$c' fzf-g$c-widget"
+      done
+    }
+    bind-git-helper f b t r h
+    unset -f bind-git-helper
   '';
 
   home.file.".zlogout".source = pkgs.writeText "zlogout" ''
