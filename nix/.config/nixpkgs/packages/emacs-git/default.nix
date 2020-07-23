4c1584c26623497e0f079feba0a1a1c610f0dece
Ruben Maher <ruben@maher.fyi>
2020-07-23 18:17:16 +0900

    fix: use updater script for emacs

--- nix/.config/nixpkgs/packages/emacs-git/default.nix
+++ nix/.config/nixpkgs/packages/emacs-git/default.nix
@@ -1,29 +1,14 @@
 { stdenv, fetchgit, emacs }:
 
-# nix-prefetch-git git://git.sv.gnu.org/emacs.git --rev emacs-26.1.92
-#
 # Check available tags:
 # http://git.savannah.gnu.org/cgit/emacs.git/refs/tags
 #
-# Create revert patch for this commit for color emoji support on macOS:
-# git revert 9344612d3cd164317170b6189ec43175757e4231
-# git show --patch HEAD > revert-9344612d3cd164317170b6189ec43175757e4231.patch
 (stdenv.lib.overrideDerivation (emacs.override { srcRepo = true; })) (attrs: rec {
   name = "emacs-${version}${versionModifier}";
   version = "28.0";
   versionModifier = ".50";
   src = fetchgit {
-    url = "git://git.sv.gnu.org/emacs.git";
-    sha256 = "1qxil0112sjr338igznzqxlj6860nmsy1ns7jki0kf7yrv3bx1hm";
-    rev = "5715eb94e90b33ace59dd4c4ccb6e2122bc6db72";
+    inherit ((builtins.fromJSON (builtins.readFile ./versions.json)).emacs) rev sha256 url;
   };
-  patches = [
-    # This patch reverts the infamous "don't use a color bitmap font until it is
-    # supported on free platforms" commit.  It is needed for 26x but already
-    # applied in master.
-    # https://news.ycombinator.com/item?id=13011185
-    # ./revert-9344612d3cd164317170b6189ec43175757e4231.patch
-    # ./clean-env.patch
-    # ./tramp-detect-wrapped-gvfsd.patch
-  ];
+  patches = [ ]; # override the parent definition
 })
